71ae4a4d7d5808fc757c3692bf75ca786f72f67c
Alfred Perlstein <alfred@freebsd.org>
2014-09-29 06:17:19 +0000

    Add initial app.

--- ghb/ghb/settings.py
+++ ghb/ghb/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
+) + ('ghb.github', )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
