f8225f03dbee10ed25b5152daabdc678dc7109b3
Alfred Perlstein <alfred@freebsd.org>
2014-09-29 04:16:41 +0000

    FreeBSD needs sqlite installed for django

--- setup.sh
+++ setup.sh
@@ -5,5 +5,13 @@
 
 
 source bin/activate
+
+# Sometimes FreeBSD doesn't have the sqlite modules, this seems
+# to fixup those broken platforms.
+if [ "$(uname -s)" = "FreeBSD" ] ; then
+    env CFLAGS=-I/usr/local/include pip install pysqlite
+fi
+
 pip install -r requirements.txt
+
 
